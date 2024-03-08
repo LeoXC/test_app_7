7b3b15785ddb3e277590ac30f71461a67697b900
Leonia Zajac <leonia.zajac@gmail.com>
2024-03-08 13:18:22 +0100

    Add Articles generated scaffold plus some changes

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
   
   root 'pages#home'
   get 'about', to: 'pages#about'
+
+  # Expose all REST-ful routes for articles
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :articles
 end
